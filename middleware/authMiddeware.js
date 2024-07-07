59e0efdc5427a62a8b0b6b2c6d5fce546c34133c
DatBuiTien18102002 <datbui18102002@gmail.com>
2024-07-07 07:29:38 +0700

    add authMiddeware

---
+++ middleware/authMiddeware.js
@@ -0,0 +1,54 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const authAdminMiddleware = (req, res, next) => {
+    const token = req.headers.token.split(' ')[1];
+    jwt.verify(token, process.env.JWT_PASS_ACCESS, function (err, user) {
+        if (err) {
+            return res.status(401).json({
+                message: err,
+                status: 'Error'
+            })
+        }
+        const { payload } = user;
+        if (payload?.isAdmin) {
+            next()
+        } else {
+            return res.status(403).json({
+                message: 'You do not have this authority',
+                status: 'Error'
+            })
+        }
+    });
+}
+
+const authUserMiddleware = (req, res, next) => {
+    const token = req.headers.token.split(' ')[1];
+    const userId = req.params.id;
+    const userBodyId = req.body?.userId;
+
+    jwt.verify(token, process.env.JWT_PASS_ACCESS, function (err, user) {
+        if (err) {
+            return res.status(401).json({
+                message: err,
+                status: 'Error'
+            })
+        }
+        const { payload } = user;
+        if (payload?.isAdmin || payload?.id === userId || payload?.id === userBodyId) {
+            next()
+        } else {
+            return res.status(403).json({
+                message: 'You do not have this authority',
+                status: 'Error'
+            })
+        }
+    });
+}
+
+module.exports = {
+    authAdminMiddleware,
+    authUserMiddleware
+}
